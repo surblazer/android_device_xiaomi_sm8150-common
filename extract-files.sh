4980a49167bcbc9227596fb9b9d9c848c9684f10
Matsvei Niaverau <matvej2002@gmail.com>
2023-07-22 15:32:09 +0200

    sm8150-common: Opt-in RIL HALs
    Change-Id: Ib677e279e88875ae54c1e19014bf862630079709

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,7 @@ if [ -z "${ONLY_FIRMWARE}" ] && [ -z "${ONLY_TARGET}" ]; then
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
     extract "${MY_DIR}/proprietary-files-fm.txt" "${SRC}" "${KANG}" --section "${SECTION}"
     extract "${MY_DIR}/proprietary-files-gnss.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files-radio.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
